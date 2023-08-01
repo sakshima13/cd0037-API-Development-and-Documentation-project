f60b1d88aef9d578bb674395b93bfe5a36d11fa5
sakshima13 <sakshi.mahajan@natwest.com>
2023-08-02 00:01:12 +0530

    updated api

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -4,20 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
 
-from models import setup_db, db, Question, Category
+from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__)
     setup_db(app)
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
-        response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type, Authorization, true')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'GET, POST, PATCH, DELETE, OPTIONS')
         return response
 
     def paginate_questions(request, questions):
@@ -33,20 +35,20 @@ def create_app(test_config=None):
     @app.route('/categories', methods=["GET"])
     def get_categories():
         if request.method == "GET":
-            categories = Category.query.order_by(Category.id).all()
+            categories = Category.query.all()
             category_data = {}
-            
+
             for data in categories:
                 category_data[data.id] = data.type
-                
+
             if len(category_data) == 0:
                 abort(404)
-            
+
             return jsonify({
                 'categories': category_data,
                 'success': True
             })
-    
+
     @app.route('/questions', methods=['GET'])
     def get_questions():
         categories = Category.query.all()
@@ -57,16 +59,16 @@ def create_app(test_config=None):
             abort(404)
 
         return jsonify({
-                "success": True,
-                "questions": list(paginated_questions_list),
-                "total_questions": len(question),
-                'categories': {item.id: item.type for item in categories}
-            })
+            "success": True,
+            "questions": list(paginated_questions_list),
+            "total_questions": len(question),
+            'categories': {item.id: item.type for item in categories}
+        })
 
     @app.route('/questions/<int:id>', methods=["DELETE"])
     def delete_question(id):
         selected_ques = Question.query.filter_by(id=id).first()
-        
+
         if selected_ques is None:
             abort(404)
         try:
@@ -75,9 +77,9 @@ def create_app(test_config=None):
                 'success': True,
                 'question': id
             })
-        except: 
+        except:
             abort(405)
-    
+
     @app.route('/questions', methods=["POST"])
     def add_question():
         try:
@@ -86,50 +88,43 @@ def create_app(test_config=None):
             new_answer = body.get('answer')
             new_category = body.get('category')
             new_difficulty = body.get('difficulty')
-            current_question = Question(
+            new_ques_detail = Question(
                 question=new_question,
                 answer=new_answer,
                 category=new_category,
                 difficulty=new_difficulty
-                )
+            )
 
-            current_question.insert()
-
-            tot_questions = Question.query.all()
-            questions_list = paginate_questions(request, tot_questions)
+            new_ques_detail.insert()
 
             return jsonify({
-            'success': True,
-            'created': current_question.id,
-            'questions': questions_list,
-            'question_created': current_question.question,
-            'total_questions': len(tot_questions)
-        })
+                'success': True
+            })
         except:
             abort(422)
 
-    @app.route('/questions/search')
+    @app.route('/questions/search', methods=["POST"])
     def search_question():
         search_term = request.args.get('searchTerm')
-        selection = Question.query.filter(Question.question.ilike(f'%{search_term}%')).all()
-        
-        print('sel', selection)
-        search_questions = paginate_questions(request, selection)
         if search_term == None:
             abort(404)
+        selected_ques_list = Question.query.filter(
+            Question.question.ilike(f'%{search_term}%')).all()
+
+        search_questions = paginate_questions(request, selected_ques_list)
 
         return jsonify({
-                "success": True,
-                "questions": list(search_questions),
-                "total_questions": len(selection),
-            })
-    
-    @app.route('/categories/<int:category_id>/questions')
-    def category_questions(category_id):
+            "success": True,
+            "questions": list(search_questions),
+            "total_questions": len(selected_ques_list),
+        })
 
+    @app.route('/categories/<int:category_id>/questions', methods=["GET"])
+    def category_questions(category_id):
         try:
-            selection = Question.query.filter(category_id == Question.category).all()
-    
+            selection = Question.query.filter(
+                category_id == Question.category).all()
+
             current_questions = paginate_questions(request, selection)
             categories = Category.query.all()
 
@@ -137,35 +132,35 @@ def create_app(test_config=None):
                 abort(404)
 
             return jsonify({
-                    "success": True,
-                    "questions": list(current_questions),
-                    "total_questions": len(selection),
-                    "current_category": [cat.type for cat in categories if cat.id == category_id ]
-                })
+                "success": True,
+                "questions": list(current_questions),
+                "total_questions": len(selection),
+                "current_category": [item.type for item in categories if item
+                                         .id == category_id]
+            })
         except:
             abort(404)
 
     @app.route('/quizzes', methods=['POST'])
-    def start_trivia():
+    def start_quiz():
         try:
             body = request.get_json()
             quiz_category = body.get('quiz_category')
             previous_questions = body.get('previous_questions')
-            category_id = quiz_category['id']
 
-            if category_id == 0:
-                questions = Question.query.filter(Question.id.notin_(previous_questions), 
-                Question.category == category_id).all()
+            if (quiz_category['id'] == 0):
+                questions = Question.query.all()
             else:
-                questions = Question.query.filter(Question.id.notin_(previous_questions), 
-                Question.category == category_id).all()
-            question = None
-            if(questions):
-                question = random.choice(questions)
+                questions = Question.query.filter(Question.id.notin_(
+                    previous_questions), Question.category == quiz_category['id']).all()
+
+            next_ques = None
+            if (len(questions) > 0):
+                next_ques = random.choice(questions).format()
 
             return jsonify({
                 'success': True,
-                'question': question.format()
+                'question': next_ques
             })
         except:
             abort(422)
@@ -204,6 +199,4 @@ def create_app(test_config=None):
             "message": "Method Not Allowed"
         }), 405
 
-
     return app
-
--- backend/models.py
+++ backend/models.py
@@ -1,28 +1,34 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-import json
 
-database_name = 'trivia'
-database_path = 'postgresql://postgres:a@{}/{}'.format('localhost:5432', database_name)
+# Connect to the database
+DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'a')
+DB_NAME = os.getenv('DB_NAME', 'trivia')
+
+DB_PATH = 'postgresql://{}:{}@{}/{}'.format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,
+)
 
 db = SQLAlchemy()
 
-"""
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-"""
-def setup_db(app, database_path=database_path):
+
+def setup_db(app, database_path=DB_PATH):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
+
 """
 Question
 
 """
+
+
 class Question(db.Model):
     __tablename__ = 'questions'
 
@@ -56,12 +62,15 @@ class Question(db.Model):
             'answer': self.answer,
             'category': self.category,
             'difficulty': self.difficulty
-            }
+        }
+
 
 """
 Category
 
 """
+
+
 class Category(db.Model):
     __tablename__ = 'categories'
 
@@ -75,4 +84,4 @@ class Category(db.Model):
         return {
             'id': self.id,
             'type': self.type
-            }
+        }
